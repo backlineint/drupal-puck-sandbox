d5020d3ba80540539f1ffc0b783bb4e2a1b5b4e9
Brian Perry <contact.brianperry@gmail.com>
2024-11-03 10:08:00 -0600

    Render puck editor on deck pages

--- web/themes/custom/revitalize/app/vite.config.js
+++ web/themes/custom/revitalize/app/vite.config.js
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    manifest: true,
+    rollupOptions: {
+      // overwrite default .html entry
+      input: ["/src/main.jsx"],
+      output: {
+        entryFileNames: `assets/[name].js`,
+        chunkFileNames: `assets/[name].js`,
+        assetFileNames: `assets/[name].[ext]`,
+      },
+    },
+  },
   plugins: [react()],
-})
+  // base: "/themes/custom/revitalize/app/dist/",
+});
